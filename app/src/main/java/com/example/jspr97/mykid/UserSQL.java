f3890869517b4f0695fae4ba0d78c231e3645265
Jasper Chin <jasper_fantasy@live.com.my>
2018-04-12 14:40:36 +0800

    - database sorting

--- app/src/main/java/com/example/jspr97/mykid/UserSQL.java
+++ app/src/main/java/com/example/jspr97/mykid/UserSQL.java
@@ -16,6 +16,8 @@ public class UserSQL {
 
     public int insert(KidActivity kidActivity) {
         SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        // insert kidActivity details into database
         ContentValues values = new ContentValues();
         values.put(KidActivity.KEY_NAME, kidActivity.getName());
         values.put(KidActivity.KEY_LOCATION, kidActivity.getLocation());
@@ -24,7 +26,7 @@ public class UserSQL {
         values.put(KidActivity.KEY_REPORTER, kidActivity.getReporter());
 
         long activity_id = db.insert(KidActivity.TABLE_NAME, null, values);
-        db.close();
+        db.close();         // close connection
         return (int) activity_id;
     }
 
@@ -46,11 +48,13 @@ public class UserSQL {
 
         db.update(KidActivity.TABLE_NAME, values, KidActivity.KEY_ID + "= ?",
                 new String[] {String.valueOf(kidActivity.getId())});
-        db.close();
+        db.close();     // close connection
     }
 
     public ArrayList<KidActivity> getKidActivityList() {
         SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        // retrieve all rows alphabetically
         String query = "SELECT " +
                 KidActivity.KEY_ID + ", " +
                 KidActivity.KEY_NAME + ", " +
@@ -58,11 +62,14 @@ public class UserSQL {
                 KidActivity.KEY_DATE + ", " +
                 KidActivity.KEY_TIME + ", " +
                 KidActivity.KEY_REPORTER +
-                " FROM " + KidActivity.TABLE_NAME;
+                " FROM " + KidActivity.TABLE_NAME +
+                " ORDER BY " + KidActivity.KEY_NAME +
+                " COLLATE NOCASE";      // sort case insensitive
 
         ArrayList<KidActivity> kidActivityList = new ArrayList<KidActivity>();
         Cursor cursor = db.rawQuery(query, null);
 
+        // initialize kidActivity objects then add to array
         if (cursor.moveToFirst()) {
             do {
                 KidActivity kidActivity = new KidActivity();
@@ -77,12 +84,14 @@ public class UserSQL {
         }
 
         cursor.close();
-        db.close();
+        db.close();         // close connection
         return kidActivityList;
     }
 
     public KidActivity getKidActivity(int activity_id) {
         SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        // retrieve row matching the id
         String query = "SELECT " +
                 KidActivity.KEY_ID + ", " +
                 KidActivity.KEY_NAME + ", " +
@@ -96,6 +105,7 @@ public class UserSQL {
         KidActivity kidActivity = new KidActivity();
         Cursor cursor = db.rawQuery(query, new String[] {String.valueOf(activity_id)});
 
+        // initialize kidActivity object
         if (cursor.moveToFirst()) {
             do {
                 kidActivity.setId(cursor.getInt(cursor.getColumnIndex(KidActivity.KEY_ID)));
@@ -108,7 +118,7 @@ public class UserSQL {
         }
 
         cursor.close();
-        db.close();
+        db.close();         // close connection
         return kidActivity;
     }
 }
